12fe96d79c97e440cb7c8b42456253aa1892b4c2
akhilmhdh <akhilmhdh@gmail.com>
2021-02-13 15:19:12 +0530

    added husky and pre-commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,14 @@ module.exports = {
         node: true,
         es6: true,
     },
-    parserOptions: { ecmaVersion: 8 }, // to enable features such as async/await
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+            modules: true,
+        },
+    },
     ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '!.prettierrc.js'], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
     extends: ['eslint:recommended'],
     overrides: [
@@ -28,19 +35,11 @@ module.exports = {
                 'plugin:prettier/recommended', // Prettier recommended rules
             ],
             rules: {
-                // We will use TypeScript's types for component props instead
                 'react/prop-types': 'off',
-
-                // No need to import React when using Next.js
                 'react/react-in-jsx-scope': 'off',
-
-                // This rule is not compatible with Next.js's <Link /> components
                 'jsx-a11y/anchor-is-valid': 'off',
-
-                // Why would you want unused vars?
                 '@typescript-eslint/no-unused-vars': ['error'],
-
-                // I suggest this setting for requiring return types on functions only where useful
+                'prettier/prettier': ['error', {}, { usePrettierrc: true }],
                 '@typescript-eslint/explicit-function-return-type': [
                     'warn',
                     {
@@ -48,7 +47,6 @@ module.exports = {
                         allowConciseArrowFunctionExpressionsStartingWithVoid: true,
                     },
                 ],
-                'prettier/prettier': ['error', {}, { usePrettierrc: true }],
             },
         },
     ],
